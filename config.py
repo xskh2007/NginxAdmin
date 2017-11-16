6d00a35a69fa74d79b7a768de607cf62fed47ec2
wangqiantu <wangqiantu@zuozh.com>
2017-11-16 11:33:33 +0800

    优化

--- config.py
+++ config.py
@@ -1,30 +1,9 @@
-base = {
-    'servers':[
-              {'index':0,
-               'name': 'server1', 
-              'host': '192.168.4.69',
-              'port': 30001,
-              'password': '',
-              'databases':16
-              },
-              {'index':1,
-               'name': 'server2', 
-              'host': '192.168.4.69',
-              'port': 30002,
-              'databases':16
-              },
-          ],
-    'seperator' : ':',
-    'maxkeylen' : 100
-}
+
 media_prefix = "pyred_media"
 
 host = '0.0.0.0'
 port = 8085
 debug = True
-
-scan_batch = 10000
-show_key_self_count = False
 
 lang = 'zh_CN'
 
